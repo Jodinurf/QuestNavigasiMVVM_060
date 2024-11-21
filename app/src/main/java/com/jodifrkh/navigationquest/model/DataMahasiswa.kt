83653268c98c1056c1fda83436e393d8e32b2c72
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 21:49:08 +0700

    membuat class DataMahasiswa yang menginisiasi data - data mahasiswa bernilai null

---
+++ app/src/main/java/com/jodifrkh/navigationquest/model/DataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.jodifrkh.navigationquest.model
+
+data class DataMahasiswa (
+    val nama: String = "",
+    val alamat: String ="",
+    val gender: String = "",
+    val noHP: String = ""
+)
