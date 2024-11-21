27a3e3e403cf0b72d9b64e94559a772e28af1ae8
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 21:48:01 +0700

    Membuat object untuk list data kelamin yang berisikan isi - isi jenis kelamin

---
+++ app/src/main/java/com/jodifrkh/navigationquest/model/DataKelamin.kt
@@ -0,0 +1,11 @@
+package com.jodifrkh.navigationquest.model
+
+import com.jodifrkh.navigationquest.R
+
+
+object DataKelamin {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
